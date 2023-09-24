90a5a7a842f41829429ea3ee2c6e49d232682d9e
MyriadProg <silv.ric700@gmail.com>
2023-09-24 22:09:10 +0300

    Exercise 1.10 Solution

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -5,20 +5,22 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 extra_payment = 1000.0
-months_paid = 1
+months_paid = 0
 
 while principal > 0:
-    if months_paid <= 12:
+    if months_paid >= extra_payment_start_month and months_paid <= extra_payment_end_month:
         principal = principal * (1+rate/12) - (payment + extra_payment)
         total_paid = total_paid + (payment + extra_payment)
         
     else:
         principal = principal * (1+rate/12) - payment
         total_paid = total_paid + payment
+    
     months_paid = months_paid + 1
-
-
-print('Total paid ', round(total_paid, 2), end='')
-print('  ', end='')
+    print (months_paid, round(total_paid, 2), round(principal, 2))
+    
+print('Total paid ', round(total_paid, 2))
 print('Months paid', months_paid)
