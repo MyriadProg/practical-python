7740e80b3eb18f506cc41f47efe3287cd7db583b
MyriadProg <silv.ric700@gmail.com>
2023-09-24 15:40:53 +0300

    Exercise 1.5 solution

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+ball_height = 100  #Initial height in meters
+num_bounces = 1
+
+while num_bounces <= 10:
+    ball_height = round(ball_height *(3/5), 4)
+    print(num_bounces, ball_height)
+    num_bounces = num_bounces + 1
+    
