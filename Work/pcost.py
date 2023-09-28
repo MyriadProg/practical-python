f3aa5a26b3d7868618951a7f5c4b3e995e2fcc87
MyriadProg <silv.ric700@gmail.com>
2023-09-28 12:07:16 +0300

    Exercise 1.27 Solution

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        stock = line.strip().split(',')
+        total_cost += int(stock[1]) * float(stock[2])
+
+print(f'Total cost {total_cost}')
