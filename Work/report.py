7a4f42acc01ecc4f365a3ae6e6f092fc17e3f5ac
MyriadProg <silv.ric700@gmail.com>
2023-09-29 23:19:52 +0300

    Exercise 2.4 Solution

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,20 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    "Reads a csv file and returns a list of tuples of (name, nshares, price)"
+
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for line in rows:
+            holding = (line[0], int(line[1]), float(line[2]))
+            portfolio.append(holding)
+    
+    return portfolio
+
+
