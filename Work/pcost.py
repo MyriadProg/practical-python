53c80abcc2487ede0656e10d4c7cbc33ca91aaab
MyriadProg <silv.ric700@gmail.com>
2023-09-29 14:15:12 +0300

    Exercise 1.32 solution

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,18 +1,21 @@
 # pcost.py
 #
-# Exercise 1.31
+# Exercise 1.32
+
+# import csv module that best works with csv files
+import csv 
 
 def portfolio_cost(filename):
     "Returns portfolio cost of filename in csv format"
 
     with open(filename, 'rt') as f:
         total_cost = 0
-        headers = next(f) # parse the header line to skip it
-        for line in f:
+        rows = csv.reader(f) # parses each line in the file into a list
+        headers = next(rows) # parse the header line to skip it
+        for line in rows:
             try:
-                stock = line.strip().split(',') # parse each line in the file into a list
-                shares = int(stock[1])
-                price = float(stock[2])
+                shares = int(line[1])
+                price = float(line[2])
                 total_cost += shares * price
             except ValueError:
                 print("Unable to process this line")
