31165142a5e0d6cfa5c16642ce6d9d7e2f59f95c
MyriadProg <silv.ric700@gmail.com>
2023-10-06 13:55:05 +0300

    Exercise 3.7 elegant solution

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -6,50 +6,38 @@ import csv
 
 def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
     '''
-    Parse a CSV file into a list of records
+    Parse a CSV file into a list of records with type conversion.
     '''
     with open(filename) as f:
         rows = csv.reader(f, delimiter=delimiter)
 
-        if has_headers:     # Executes if the csv file has headers
-            # Read the file headers
-            headers = next(rows)
+        # Read the file headers (if any)
+        headers = next(rows) if has_headers else []
 
-            # If a column selector was given, find indices of the specified columns
-            # Also narrow the set of headers used for resulting dictionaries
-            if select:
+        # If a column selector was given, find indices of the specified columns
+        # Also narrow the set of headers used for resulting dictionaries
+        if select:
                 indices = [headers.index(colname) for colname in select]
                 headers = select
-            else:
-                indices  = []
+         
+        records = []
+        for row in rows:
+            if not row:     # Skip rows with no data
+                continue
 
-            records = []
-            for row in rows:
-                if not row:         # Skip rows with no data
-                    continue
-
-                # Filter the row if specific columns were selected
-                if indices:
-                    row = [ row[index] for index in indices ]
+            # Filter the row if specific columns were selected
+            if select:
+                row = [ row[index] for index in indices ]
+            
+            # Perform type conversion according to the specified types
+            if types:
+                row = [ func(val) for func, val in zip(types, row) ]
                 
-                # Perform type conversion according to the specified types
-                if types:
-                    row = [ func(val) for func, val in zip(types, row) ]
-                # Make a dictionary
+            # Make a dictionary or a tuple
+            if headers:
                 record = dict(zip(headers, row))
-                records.append(record)
-        
-        else:       # Executes if the csv file doesn't have headers
-            records = []
-            for row in rows:
-                if not row:         # Skip rows with no data
-                    continue
-                
-                # Perform type conversion according to the specified types
-                if types:
-                    row = [ func(val) for func, val in zip(types, row)]
-                # Make a tuple
+            else:
                 record = tuple(row)
-                records.append(record)
+            records.append(record)
 
     return records
