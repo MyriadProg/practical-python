c7199d3707acc58ae799caeb57a76266fb1e1367
MyriadProg <silv.ric700@gmail.com>
2023-11-05 00:01:06 +0300

    Exercise 6.7 Solution

--- Work/follow.py
+++ Work/follow.py
@@ -1,10 +1,13 @@
-# Exercise 6.6
+# Exercise 6.7
 # follow.py
 
 import os
 import time
 
 def follow(filename):
+    '''
+    Read lines at the end of a file
+    '''
     f = open(filename)
     f.seek(0, os.SEEK_END) # Move file pointer 0 bytes from end of file
 
@@ -16,10 +19,14 @@ def follow(filename):
         yield line
 
 if __name__ == '__main__':
+    import report
+
+    portfolio = report.read_portfolio('Data/portfolio.csv')
+
     for line in follow('Data/stocklog.csv'):
         fields = line.split(',')
         name = fields[0].strip('"')
         price = float(fields[1])
         change = float(fields[4])
-        if change < 0:
+        if name in portfolio:
             print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
