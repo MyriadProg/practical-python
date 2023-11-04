b17b2d8d71090d7558b147c452e011c37d054439
MyriadProg <silv.ric700@gmail.com>
2023-11-04 21:36:41 +0300

    Exercise 6.3 Solution

--- Work/portfolio.py
+++ Work/portfolio.py
@@ -1,4 +1,4 @@
-# Exercise 6.2
+# Exercise 6.3
 
 class Portfolio:
 
@@ -7,6 +7,15 @@ class Portfolio:
     
     def __iter__(self):
         return self._holdings.__iter__()
+    
+    def __len__(self):
+        return len(self._holdings)
+    
+    def __getitem__(self, index):
+        return self._holdings[index]
+    
+    def __contains__(self, name):
+        return any([s.name == name for s in self._holdings])
     
     @property
     def total_cost(self):
