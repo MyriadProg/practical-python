309347255d9277c1595e39c215f0101694756676
MyriadProg <silv.ric700@gmail.com>
2023-11-04 19:58:30 +0300

    Exercise 5.7 and 5.8 Solution

--- Work/stock.py
+++ Work/stock.py
@@ -1,6 +1,7 @@
 # Exercise 4.2
 
 class Stock:
+    __slots__=('name', '_shares', 'price')
     def __init__(self, name, shares, price) -> None:
         self.name = name
         self.shares = shares
@@ -9,6 +10,16 @@ class Stock:
     @property
     def cost(self):
         return self.shares * self.price
+    
+    @property
+    def shares(self):
+        return self._shares
+    
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
     
     def sell(self, num_shares):
         self.shares -= num_shares
